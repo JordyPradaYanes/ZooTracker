bc81537a9a64ed287ace7757611786e1425b3376
JordyPradaYanes <jdpraday@ufpso.edu.co>
2025-02-28 22:08:36 -0500

    SCRUM-4 Crear Pagina de Inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
+import { FeaturesComponent } from './features/features.component';
+import { FooterComponent } from './footer/footer.component';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    HeaderComponent,
+    HeroComponent,
+    FeaturesComponent,
+    FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ZooTracker';
 }
+
+
