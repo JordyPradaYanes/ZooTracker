1e23062f14885d1872c38211f4532d6d1e07a5ef
LuisaFernandaRinconOrtiz <115329979+LuisaFernandaRinconOrtiz@users.noreply.github.com>
2025-03-08 10:38:41 -0500

    Revert "Luisa soa"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
+import { FeaturesComponent } from './features/features.component';
+import { FooterComponent } from './footer/footer.component';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    HeaderComponent,
+    HeroComponent,
+    FeaturesComponent,
+    FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
