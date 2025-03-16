11480cb3f53cc8d89f198076da2c5876be2f14cc
JordyPradaYanes <jdpraday@ufpso.edu.co>
2025-03-15 23:55:38 -0500

    add Inventario y registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 
 import {LoginComponent} from './login/login.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { InventarioComponent } from './inventario/inventario.component';
+import { RegistrosComponent } from './registros/registros.component';
 
 export const routes: Routes = [
 	{path: '', component: MainPageComponent},
 	{path: 'regresar', component: MainPageComponent},
 	{path: 'login', component: LoginComponent},
+	{path: 'inventario', component: InventarioComponent},
+	{path: 'registros', component: RegistrosComponent},
 	{ path: '**', redirectTo: '' }
 ];
